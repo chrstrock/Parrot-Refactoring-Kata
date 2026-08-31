153de6d15ac39934e875efd6cba157abf2d48a8f
David Ferrell <chrstrock@gmail.com>
2026-08-31 16:53:47 -0400

    r refactor: move createParrot to ParrotTypeEnum class.

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -12,14 +12,6 @@ public abstract class Parrot {
         this.isNailed = isNailed;
     }
 
-    public static Parrot createParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        return switch (type) {
-            case EUROPEAN -> new EuropeanParrot(numberOfCoconuts, voltage, isNailed);
-            case AFRICAN -> new AfricanParrot(numberOfCoconuts, voltage, isNailed);
-            case NORWEGIAN_BLUE -> new NorwegianBlueParrot(numberOfCoconuts, voltage, isNailed);
-        };
-    }
-
     public abstract double getSpeed();
 
     protected double getBaseSpeed(double voltage) {
--- Java/src/main/java/parrot/ParrotTypeEnum.java
+++ Java/src/main/java/parrot/ParrotTypeEnum.java
@@ -4,5 +4,13 @@ public enum ParrotTypeEnum {
 
     EUROPEAN,
     AFRICAN,
-    NORWEGIAN_BLUE
+    NORWEGIAN_BLUE;
+
+    public static Parrot createParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        return switch (type) {
+            case EUROPEAN -> new EuropeanParrot(numberOfCoconuts, voltage, isNailed);
+            case AFRICAN -> new AfricanParrot(numberOfCoconuts, voltage, isNailed);
+            case NORWEGIAN_BLUE -> new NorwegianBlueParrot(numberOfCoconuts, voltage, isNailed);
+        };
+    }
 }
