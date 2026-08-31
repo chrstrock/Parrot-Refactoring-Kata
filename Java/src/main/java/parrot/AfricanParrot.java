c95161ef8cfe616668dc4ab894c54717a7548f8f
David Ferrell <chrstrock@gmail.com>
2026-08-31 16:47:57 -0400

    remove unused type from super class.

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -1,10 +1,8 @@
 package parrot;
 
-import static parrot.ParrotTypeEnum.AFRICAN;
-
 public class AfricanParrot extends Parrot {
     protected AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(AFRICAN, numberOfCoconuts, voltage, isNailed);
+        super(numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -1,10 +1,8 @@
 package parrot;
 
-import static parrot.ParrotTypeEnum.EUROPEAN;
-
 public class EuropeanParrot extends Parrot {
         public EuropeanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(EUROPEAN, numberOfCoconuts, voltage, isNailed);
+        super(numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
