068ade359b18212b8a17ec8682b7682be234a294
David Ferrell <chrstrock@gmail.com>
2026-08-31 16:43:53 -0400

    refactor European getCry method

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -1,8 +1,10 @@
 package parrot;
 
+import static parrot.ParrotTypeEnum.EUROPEAN;
+
 public class EuropeanParrot extends Parrot {
         public EuropeanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(ParrotTypeEnum.EUROPEAN, numberOfCoconuts, voltage, isNailed);
+        super(EUROPEAN, numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
@@ -12,10 +14,6 @@ public class EuropeanParrot extends Parrot {
 
     @Override
     public String getCry() {
-        return switch (type) {
-            case EUROPEAN -> "Sqoork!";
-            case AFRICAN -> "Sqaark!";
-            case NORWEGIAN_BLUE -> voltage > 0 ? "Bzzzzzz" : "...";
-        };
+        return "Sqoork!";
     }
 }
